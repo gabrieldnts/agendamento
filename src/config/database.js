04cd939d89f6075ae831ec738cccb4cac12d2a5d
Gabriel Dantas <gabriel.dantas@pmm.am.gov.br>
2019-08-06 12:35:03 -0400

    Deploy no Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   dialect: process.env.DB,
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
